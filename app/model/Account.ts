f35a8c33205a69b9acde963c4206bea9753c55ee
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-21 14:14:06 -0500

    created an interface for accounts

---
+++ app/model/Account.ts
@@ -0,0 +1,16 @@
+
+
+
+export interface InerfaceAccount extends Document {
+    _id:number
+    firstname:string
+    lastname:string
+    middleInit:string
+    age:number
+    email:string
+    number:string
+    created_at:Date
+    updated_at:Date
+}
+
+const account = new mongoose.Schema
