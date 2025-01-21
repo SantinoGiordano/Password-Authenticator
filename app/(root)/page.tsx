0d6cf1f8a8b89ebeffe0a0924093ac3b6ec141d5
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-21 14:49:09 -0500

    setup heading for landing page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
 export default function Home() {
   return (
     <>
-      <div>
-        <p className="p-5">
+      <div className="text-center">
+        <div>
+        <p className="p-5 text-2xl font-bold ">
           Hello and welcome, please sign in to an account, if you dont have an
           account already, feel free to create an account
         </p>
+        </div>
       </div>
     </>
   );
