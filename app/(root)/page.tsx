d7fc0639016c454aab993e03459a12cf0c3e6961
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-21 14:41:45 -0500

    setup landing page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
-
-
 export default function Home() {
   return (
-<p>
-  Hello
-</p>
+    <>
+      <div>
+        <p className="p-5">
+          Hello and welcome, please sign in to an account, if you dont have an
+          account already, feel free to create an account
+        </p>
+      </div>
+    </>
   );
 }
