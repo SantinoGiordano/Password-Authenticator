ae001c44e1fce8382553afdde4581238ceeb6d8f
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-22 14:37:39 -0500

    just a button for testing purposes, will get deleted later in development

--- app/(root)/userLandingPage/page.tsx
+++ app/(root)/userLandingPage/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function UserLandingPage(){
-    return <div>Hello World</div>
+    return(
+        <>
+        <div>Hello World</div>
+        <Link href={'./'}>Home</Link>
+        </>
+    ) 
+
 }
