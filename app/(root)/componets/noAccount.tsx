2ea9c9f2401f5ce5ab4cbbec5e200817ee97a152
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-22 15:04:27 -0500

    componet for when theres no account

---
+++ app/(root)/componets/noAccount.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const noAccount = () => {
+  return (
+    <>
+      <p> It seems like you dont have an account with us yet </p>
+      
+    </>
+  );
+};
+
+export default noAccount;
