3e6d0c91cfe50dbd22c2872b12400e8f103f7420
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-22 14:09:57 -0500

    added my routers to push to userLanding PAge

--- app/(root)/signIn/page.tsx
+++ app/(root)/signIn/page.tsx
@@ -1,16 +1,24 @@
+'use client'
+
+import Router from "next/router";
+
 export default function SignIn(){
 
     const Admin = true;
+    const router = useRouter();
 
     function AuthUser(){
-
+        if(Admin == true){
+            Router.push('/userLandingPage'); 
+        }
+        else{
+            console.log('there was a problem')
+        }
     }
 
 
     return(
-       <button>Sign In</button>
+       <button onClick={()=>AuthUser()}>Sign In</button>
         
     ) 
-
-    
 }
